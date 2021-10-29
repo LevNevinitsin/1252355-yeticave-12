0d1260cbcc31739e597ca4188f7682f260acf18e
Lev <levnevinitsin@gmail.com>
2021-10-29 10:37:59 +0300

    Apply reading and displaying records from MySQL
    Add config sample file for conntecting to database.
    
    Update database dump file, as items' expire dates had been updated as
    well as new item and new category had been added.

--- templates/main.php
+++ templates/main.php
@@ -3,8 +3,8 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
         <?php foreach ($categories as $category): ?>
-        <li class="promo__item promo__item--boards">
-            <a class="promo__link" href="pages/all-lots.html"><?= esc($category) ?></a>
+        <li class="promo__item promo__item--<?= esc($category['category_code']) ?>">
+            <a class="promo__link" href="pages/all-lots.html"><?= esc($category['category_name']) ?></a>
         </li>
         <?php endforeach ?>
     </ul>
@@ -14,20 +14,20 @@
         <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-        <?php foreach ($offers as $offer): ?>
+        <?php foreach ($items as $item): ?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?= esc($offer['img_url']) ?>" width="350" height="260" alt="<?= esc($offer['name']) ?>">
+                <img src="<?= esc($item['item_image']) ?>" width="350" height="260" alt="<?= esc($item['item_name']) ?>">
             </div>
             <div class="lot__info">
-                <span class="lot__category"><?= esc($offer['category']) ?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= esc($offer['name']) ?></a></h3>
+                <span class="lot__category"><?= esc($item['category_name']) ?></span>
+                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= esc($item['item_name']) ?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?= esc(formatPrice($offer['price'])) ?></span>
+                        <span class="lot__cost"><?= esc(formatPrice($item['item_initial_price'])) ?></span>
                     </div>
-                    <?php list ($hours, $minutes) = getRemainingTime($offer['expire_date']); ?>
+                    <?php list ($hours, $minutes) = getRemainingTime($item['item_date_expire']); ?>
                     <div class="lot__timer timer <?= ($hours === '00') ? 'timer--finishing' : '' ?>">
                         <?= esc($hours . ":" . $minutes) ?>
                     </div>
