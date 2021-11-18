c11ce60205cf1a37a55a9c41373752bce71f0f8f
Lev <levnevinitsin@gmail.com>
2021-11-18 17:31:37 +0300

    Add and apply template for lots list

--- templates/search.php
+++ templates/search.php
@@ -10,33 +10,11 @@
 <div class="container">
     <section class="lots">
         <h2>Результаты поиска по запросу «<span><?= esc($searchString) ?></span>»</h2>
-        <ul class="lots__list">
-            <?php if ($foundItems): ?>
-                <?php foreach($foundItems as $item): ?>
-                <li class="lots__item lot">
-                    <div class="lot__image">
-                        <img src="<?= esc($item['item_image']) ?>" width="350" height="260" alt="<?= esc($item['item_name']) ?>">
-                    </div>
-                    <div class="lot__info">
-                        <span class="lot__category"><?= esc($item['category_name']) ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="/lot.php?item_id=<?= esc($item['item_id']) ?>"><?= esc($item['item_name']) ?></a></h3>
-                        <div class="lot__state">
-                            <div class="lot__rate">
-                                <span class="lot__amount"><?= getBidsCountText($item['bids_count']) ?></span>
-                                <span class="lot__cost"><?= esc(formatPrice($item['current_price'])) ?></span>
-                            </div>
-                            <?php list ($hoursCount, $minutesCount, $secondsCount) = getRemainingTime($item['item_date_expire']); ?>
-                            <div class="lot__timer timer">
-                                <?= esc("$hoursCount:$minutesCount:$secondsCount") ?>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <?php endforeach ?>
-            <?php else: ?>
-                <p><?= esc($searchMessage) ?></p>
-            <?php endif ?>
-        </ul>
+        <?php if ($foundItems): ?>
+        <?= includeTemplate('items-list.php', ['items' => $foundItems])?>
+        <?php else: ?>
+        <p><?= esc($searchMessage) ?></p>
+        <?php endif ?>
     </section>
     <?php if ($pagesCount > 1): ?>
     <ul class="pagination-list">
